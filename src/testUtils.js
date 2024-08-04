6219fa686b58cd283e1dc79bb08970c4e164c3b1
jcastroctrlplay <j.castro@ctrlplay.com.br>
2024-08-04 19:23:14 -0300

    feat: 🎸 configuring environment

--- src/testUtils.js
+++
@@ -1,9 +0,0 @@
-import { MemoryRouter } from "react-router-dom";
-import { render } from "@testing-library/react";
-
-const wrapper = ({ children }) => <MemoryRouter>{children}</MemoryRouter>;
-
-const customRender = (ui, options) => render(ui, { wrapper, ...options });
-
-export * from  '@testing-library/react'
-export { customRender as render };
