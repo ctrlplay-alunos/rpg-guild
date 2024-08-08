61796bdfbea2aeded043843b66b6a9e536b326ad
jcastroctrlplay <j.castro@ctrlplay.com.br>
2024-08-07 23:25:58 -0300

    feat: 🎸 using app for view radom dog

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import { Guilds } from "./components/guilds";
 import { Members } from "./components/members";
 import { MemberForm } from "./components/memberForm";
 import { GuildForm } from "./components/guildForm";
+import { RandomDog } from "./components/challenge/lesson-03/randomDog";
 
 const App = () => (
   <Router>
     <div className="bg-[#ECEAE0] w-screen h-screen">
       <Navbar />
+      <RandomDog />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/guilds" element={<Guilds />} />
